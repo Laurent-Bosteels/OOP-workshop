5499d2fdd206cb8d13dc4d8c94e049963e4a75fb
Laurent Bosteels <laurent.bosteels@gmail.com>
2021-05-26 15:07:07 +0200

    added visibility exercise

---
+++ visibility.php
@@ -0,0 +1,95 @@
+
+<?php
+
+class Beverage
+
+{
+
+    protected string $color;
+    protected float $price;
+    protected string $temperature = "cold";
+
+    public function __construct(string $color, float $price, string $temperature = "cold")
+
+    {
+
+        $this->color = $color;
+        $this->price = $price;
+        $this->temperature = $temperature;
+    }
+
+    public function getInfo()
+
+    {
+
+        return "This beverage is " . $this->temperature . " and " . $this->color . " and costs " . $this->price . " euro";
+    }
+
+    public function setColor($color)
+
+    {
+
+        $this->color = $color;
+    }
+
+    public function getColor()
+    {
+        return $this->color;
+    }
+
+
+}
+
+class Beer extends Beverage
+
+{
+    protected string $name;
+    protected float $alcoholPercentage;
+
+    public function __construct(string $color, float $price, string $name, float $alcoholPercentage, string $temperature = "cold")
+
+    {
+        $this->name = $name;
+        $this->alcoholPercentage = $alcoholPercentage;
+        parent::__construct($color, $price, $temperature);
+    }
+
+    public function getAlcoholpercentage()
+
+    {
+        return $this->alcoholPercentage . "% alcoholpercentage";
+    }
+    public function getName(): string
+
+    {
+
+        return $this->name;
+    }
+
+    public function getInfo()
+
+    {
+        return parent::getInfo() . " and has a " . $this->alcoholPercentage . "% alcoholpercentage" . " and I have a " . $this->getColor() . " color";
+    }
+
+    public function beerInfo()
+    {
+        return "Hi, I'm " . $this->name . " and I have an alcoholic percentage of " . $this->alcoholPercentage . "";
+    }
+}
+
+$duvel = new Beer("blond", 3.5, "Duvel", 8.5);
+
+echo "<br>";
+echo $duvel->getInfo();
+echo "<br>";
+echo $duvel->getAlcoholpercentage();
+echo "<br>";
+
+$duvel->setColor('light');
+echo $duvel->getInfo();
+echo "<br>";
+echo $duvel->beerInfo();
+
+
+var_dump($duvel);
